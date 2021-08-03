433ed0f37ddff4487fa39c44140ecdf5def995b2
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 16:05:58 +0300

    Add files via upload

---
+++ Lambda/Lambda/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Lambda
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] seq = Console.ReadLine().Split().Select(int.Parse).ToArray();          
+            for (int i = 1; i <= n; i++)
+            {               
+                bool divisable = true;
+                for (int j = 0; j < seq.Length; j++)
+                {
+                    Predicate<int> dividable = n => n % seq[j] == 0;
+                    if (!dividable(i))
+                    {
+                        divisable = false;
+                    }
+                }
+                if (divisable)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }    
+    }
+}
