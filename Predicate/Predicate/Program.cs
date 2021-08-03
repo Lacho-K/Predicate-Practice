8d20d0084942f280c34c2b0d698310dd0f2427f3
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 15:28:45 +0300

    Add files via upload

---
+++ Predicate/Predicate/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+namespace Predicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> firstLetterCap = l => l[0] >= 65 && l[0] <= 90;
+            Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Where(x => firstLetterCap(x)).ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
